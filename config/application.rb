a1fa39e8765cb20d93fbb6b236de9c688c827a7c
NVTHANH229 <nvthanh229@gmail.com>
2023-05-24 20:45:50 +0700

    fixbloghosting

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module Demo8
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << "demo8-production.up.railway.app"
   end
 end
